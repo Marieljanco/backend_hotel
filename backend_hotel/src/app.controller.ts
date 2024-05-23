3c5f998fd956e6de5480c5ce2baa404f4801dc13
marieljanco <jancoa795@gmail.com>
2024-05-22 23:10:12 -0400

    packjson

--- backend_hotel/src/app.controller.ts
+++ backend_hotel/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('saludar')
+  saludar(): string {
+    return this.appService.saludar();
+  }
+
+  @Post('saludar')
+  saludarPost(): string {
+    return this.appService.saludar();
   }
 }
